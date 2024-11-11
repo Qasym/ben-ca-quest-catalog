e3f23f7e8c156220e52eba5ed20d4fd556ec98d2
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-11 17:58:48 +0900

    added api for quest retrieval for users: with reward info

--- src/main/kotlin/com/kasymzhan/quest/catalog/controller/QuestCatalogController.kt
+++ src/main/kotlin/com/kasymzhan/quest/catalog/controller/QuestCatalogController.kt
@@ -2,19 +2,54 @@ package com.kasymzhan.quest.catalog.controller
 
 import com.kasymzhan.quest.catalog.data.Quest
 import com.kasymzhan.quest.catalog.data.QuestRequest
+import com.kasymzhan.quest.catalog.data.QuestResponse
+import com.kasymzhan.quest.catalog.data.QuestReward
 import com.kasymzhan.quest.catalog.repository.QuestRepository
+import com.kasymzhan.quest.catalog.service.JwtTokenService
+import jakarta.servlet.http.HttpServletRequest
 import org.bson.types.ObjectId
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 
 @RestController
 @RequestMapping("/quests")
-class QuestCatalogController(val questRepository: QuestRepository) {
+class QuestCatalogController(
+    val questRepository: QuestRepository,
+    val webClient: WebClient,
+    val tokenService: JwtTokenService
+) {
+    @GetMapping
+    fun getQuests(httpReq: HttpServletRequest): List<QuestResponse> {
+        val authentication = SecurityContextHolder.getContext().authentication
+        val token = tokenService.tryParseToken(httpReq) ?: println("accessing without token")
+        val quests = questRepository.findAll()
+        val url = "http://localhost:2004/rewards"
+        val rewardList: List<QuestReward> = webClient.get()
+            .uri(url)
+            .header("Authorization", "Bearer $token")
+            .retrieve()
+            .bodyToMono(object : ParameterizedTypeReference<List<QuestReward>>() {})
+            .block() ?: emptyList()
+        val rewardMap = rewardList.associateBy { it.id }
+        return try {
+            quests.map {
+                val reward = rewardMap[it.rewardId.toString()] ?: throw Exception("Reward not found")
+                it.toQuestResponse(reward)
+            }
+        } catch (e: Exception) {
+            println("exception $e")
+            emptyList()
+        }
+    }
+
     @GetMapping("/get/all")
     fun getAllQuests(): List<Quest> {
         return questRepository.findAll()
@@ -36,5 +71,14 @@ class QuestCatalogController(val questRepository: QuestRepository) {
             duplication = this.duplication,
             autoClaim = this.autoClaim,
             rewardId = this.rewardId
+        )
+
+    private fun Quest.toQuestResponse(reward: QuestReward): QuestResponse =
+        QuestResponse(
+            name = this.name,
+            description = this.description,
+            duplication = this.duplication,
+            streak = this.streak,
+            reward = reward,
         )
 }
