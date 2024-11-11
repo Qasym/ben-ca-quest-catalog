8b1212028e631ddd0023e41639c429a0d2e3479c
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-11 09:05:12 +0900

    added jwt support

---
+++ src/main/kotlin/com/kasymzhan/quest/catalog/config/JwtAuthenticationFilter.kt
@@ -0,0 +1,59 @@
+package com.kasymzhan.quest.catalog.config
+
+import com.kasymzhan.quest.catalog.service.JwtTokenService
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtAuthenticationFilter(private val tokenService: JwtTokenService) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = tryParseToken(request)
+        if (tokenService.isValid(token)) {
+            val claims = tokenService.getAllClaims(token!!)
+            val roles = token.getRoles()
+            val auth = UsernamePasswordAuthenticationToken(claims.subject, null, roles)
+            auth.details = WebAuthenticationDetailsSource().buildDetails(request)
+            SecurityContextHolder.getContext().authentication = auth
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    private fun tryParseToken(request: HttpServletRequest): String? {
+        val authHeader: String? = request.getHeader("Authorization")
+        if (!authHeader.containsToken())
+            return null
+        val token = authHeader!!.extractToken()
+        return token
+    }
+
+    private fun String?.containsToken() =
+        this != null && this.startsWith("Bearer ")
+
+    private fun String.extractToken(): String =
+        this.substringAfter("Bearer ")
+
+    private fun String.getRoles(): List<SimpleGrantedAuthority> {
+        val claims = tokenService.getAllClaims(this)
+        return try {
+            val roles = claims["roles"] as List<*>
+            roles.mapNotNull {
+                val roleName = (it as? Map<*, *>)?.get("authority") as? String
+                roleName?.let { SimpleGrantedAuthority("$roleName") }
+            }
+        } catch (e: Exception) {
+            println("exception: $e")
+            emptyList()
+        }
+    }
+}
---
+++ src/main/kotlin/com/kasymzhan/quest/catalog/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.kasymzhan.quest.catalog.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(val jwtAuthenticationFilter: JwtAuthenticationFilter) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers("/quests").hasRole("USER")
+                    .requestMatchers("/quests/add", "/quests/get/all").hasRole("ADMIN")
+                    .anyRequest().fullyAuthenticated()
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .build()
+}
